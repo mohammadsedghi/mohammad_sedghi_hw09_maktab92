e0acbf03f50279b21a0f681bd8ead3ce91cd9d45
mohammad <mohammadsedghi1993@gmail.com>
2023-04-02 20:17:54 +0430

    class user added constructor,setter,getter

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,83 @@
 package entity;
 
 public class User {
+    String name;
+    String family;
+    String phoneNumber;
+    String  emailAddress;
+    String passWord;
+    Address address;
+
+    public User(String name, String family, String phoneNumber, String emailAddress, String passWord, Address address) {
+        this.name = name;
+        this.family = family;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.passWord = passWord;
+        this.address = address;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", family='" + family + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", passWord='" + passWord + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
 }
